63df6798b1282e862f6622ec38d5d75233577f89
Daniel  Morton <dacmorton@gmail.com>
2025-01-13 19:42:56 -0500

    clippy

--- src/loc.rs
+++ src/loc.rs
@@ -3,7 +3,6 @@ use polars::prelude::{DataFrame, LazyCsvReader, LazyFileListReader};
 /**
 Loads the csv consisting of all locations for which data is to be scraped.
 */
-
 pub(super) fn load_data(loc_file: &str) -> DataFrame {
     LazyCsvReader::new(loc_file)
         .with_has_header(true)
